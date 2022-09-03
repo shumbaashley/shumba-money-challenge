fa70f167bbec6b9c8a72211776eef3b6fbd6dae4
shumbaashley <ashleytshumba@gmail.com>
2022-09-03 11:43:19 +0200

    smiling lion image

--- client/src/pages/sign-in/index.js
+++ client/src/pages/sign-in/index.js
@@ -6,12 +6,12 @@ import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import SmilingLionImage from "../../assets/images/Smiling-Lion.jpg";
 
 function Copyright(props) {
   return (
@@ -53,7 +53,7 @@ export default function SignInSide() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundImage: `url(${SmilingLionImage})`,
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
@@ -63,7 +63,7 @@ export default function SignInSide() {
             backgroundPosition: "center",
           }}
         />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid item xs={12} sm={8} md={5} square>
           <Box
             sx={{
               my: 8,
