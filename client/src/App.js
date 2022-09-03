dfc2fd0777a7c2e873075fb5d698729ddfdbcbc6
shumbaashley <ashleytshumba@gmail.com>
2022-09-03 11:18:58 +0200

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SignInSide from "./pages/sign-in";
 
 function Home() {
   return <h2>Home</h2>;
@@ -16,29 +17,11 @@ function Users() {
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
           <Route path="/about" element={<About />}></Route>
-          <Route path="/users" element={<Users />}></Route>
+          <Route path="/login" element={<SignInSide />}></Route>
           <Route path="/" element={<Home />}></Route>
         </Routes>
-      </div>
     </Router>
   );
 }
